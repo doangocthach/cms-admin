516953154ae392d2a425c517cba62ec4c4692b3d
tung1203 <tung120320@gmail.com>
2020-07-30 14:51:57 +0700

    fix date

--- src/components/CampaignInfomation.js
+++ src/components/CampaignInfomation.js
@@ -269,7 +269,7 @@ export default function EnhancedTable(props) {
   const getGaTrafficByDay = gql`
     query($campaignId: String) {
       getGaTrafficByDay(campaignId: $campaignId) {
-        day
+        date
         numberOfUser
       }
     }
